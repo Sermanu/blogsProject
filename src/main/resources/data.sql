05d70a0df2173485c5adb7d8f37c116495aa2895
Sermanu <smtt_1995@hotmail.com>
2022-02-10 19:36:13 -0500

    fix values un script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,5 +7,5 @@ INSERT INTO author (name,email,phone, birthdate) VALUES ('Ana Perez', 'aperez@gm
 
 INSERT INTO author (name,email,phone, birthdate) VALUES ('Juan Tovar', 'jtovar@gmail.com', '994567123', '1990-02-13T14:00');
 INSERT INTO blog (name, description, url, status, author_id) VALUES('Blog de JT', 'Temas de politica', 'https://blogprueba.com', 'inactivo', 3);
-INSERT INTO post (title, postdate, status, content, blog_id) VALUES('Post de JT', '1995-04-25T16:00', 'borrador', 'contenido de prueba', 3);
-INSERT INTO comment (commentdate, name, estado, post_id) VALUES('2000-03-15T11:00', 'Comentario de JZ', 'estado de prueba', 3);
+INSERT INTO post (title, postdate, status, content, blog_id) VALUES('Post de JT', '1995-04-25T16:00', 'borrador', 'contenido de prueba', 2);
+INSERT INTO comment (commentdate, name, estado, post_id) VALUES('2000-03-15T11:00', 'Comentario de JZ', 'estado de prueba', 2);
